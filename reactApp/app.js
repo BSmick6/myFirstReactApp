c091b53017d81f3a99f7745a2bec181ed193fc93
Brock <bws219@lehigh.edu>
2017-10-06 15:35:33 -0700

    added input field and cross out of tasks

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const dummyData = ["Come to San Fran", "Learn React", "Profit"];
+const dummyData = [
+  { taskText: "Come to San Fran", completed: false },
+  { taskText: "Learn React", completed: true },
+  { taskText: "Profit", completed: false }
+];
+
+class InputLine extends React.Component {
+  render() {
+    return (
+      <div>
+        <input placeholder="Task..." />
+        <button>Add todo</button>
+      </div>
+    )
+  }
+}
 
 class Todo extends React.Component {
   render() {
+    const t = this.props.task;
     return (
-      <li><button>X</button>{this.props.task}</li>
+      <li>
+        <button>X</button>
+        {this.props.completed ? <strike>{t}</strike> : t}</li>
     )
   }
 }
@@ -15,10 +33,21 @@ class TodoList extends React.Component {
   render() {
     return (
       <ul>
-        {dummyData.map((todo) => (<Todo task={todo} />))}
+        {dummyData.map((todo) => (<Todo task={todo.taskText} completed={todo.completed} />))}
       </ul>
     )
   }
 }
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+class TodoApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <InputLine />
+        <TodoList />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
