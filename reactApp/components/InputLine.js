9a30fe7001e2ad590ccafe4f0fdffda81beee5b3
Brock <bws219@lehigh.edu>
2017-10-06 17:08:34 -0700

    added input on click relative to text box

--- reactApp/components/InputLine.js
+++ reactApp/components/InputLine.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
-function InputLine(props) {
-  return (
-    <div>
-      <input placeholder="Task..." />
-      <button onClick={() => props.submit("Test Task")}>Add todo</button>
-    </div>
-  );
+class InputLine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      typedText: ''
+    }
+  }
+
+  handleTyping(event) {
+    this.setState({ typedText: event.target.value });
+  }
+
+  handleSubmit() {
+    this.props.submit(this.state.typedText);
+    this.setState({ typedText: '' })
+  }
+
+  render() {
+    return (
+      <div>
+        <input placeholder="Task..." onChange={(e) => this.handleTyping(e)} value={this.state.typedText} />
+        <button onClick={() => this.handleSubmit()}>Add todo</button>
+      </div>
+    );
+  }
 };
 
 export default InputLine;
